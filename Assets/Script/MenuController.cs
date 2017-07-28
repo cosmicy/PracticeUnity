ffffd74bcf6ae2df685326eabd706611cc3cec78
cytdqs <cosmicy@outlook.com>
2017-07-28 16:06:06 +0800

    *在player上添加脚本，指定脚本里变量的值

---
+++ Assets/Script/MenuController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public Color pink;
+
+    public SkinnedMeshRenderer headRender;
+    public Mesh[] headMeshArray;
+    private int headMeshIndex = 0;
+
+
+
+	public void OnHeadMeshNext() {
+
+	}
+
+	public void OnHandMeshNext() {
+
+	}
+
+	public void OnChangeColorBlue() {
+		OnChangeColor(Color.blue);
+	}
+
+	public void OnChangeColorCyan() {
+        OnChangeColor(Color.cyan);
+	}
+	public void OnChangeColorGreen() {
+        OnChangeColor(Color.green);
+	}
+	public void OnChangeColorPink() {
+        OnChangeColor(pink);
+	}
+	public void OnChangeColorRed() {
+        OnChangeColor(Color.red);
+	}
+
+
+	private void OnChangeColor(Color c) {
+
+	}
+
+	public void OnPlay() {
+		
+	}
+}
