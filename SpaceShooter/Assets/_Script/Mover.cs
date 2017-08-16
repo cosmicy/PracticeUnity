711ac31454872ce362f3fe79bbcb207b0405b3a6
cosmi <cosmicy@outlook.com>
2017-08-16 21:21:52 +0800

    *电光子弹制作完成

---
+++ SpaceShooter/Assets/_Script/Mover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+	public float speed = 4.0f;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody> ().velocity = transform.forward * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
