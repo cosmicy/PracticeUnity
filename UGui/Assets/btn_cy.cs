2efbbaba03658eff5ec5d2c61ad7c7198107341a
cosmi <cosmicy@outlook.com>
2017-08-24 21:02:23 +0800

    *不规则图片按钮http://blog.csdn.net/shenmifangke/article/details/53504036

---
+++ UGui/Assets/btn_cy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class btn_cy : MonoBehaviour {
+
+
+	public float threshold = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		Image image = GetComponent<Image> ();
+		//image.eventAlphaThreshold = threshold;
+		image.alphaHitTestMinimumThreshold = threshold;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Click()
+	{
+		Debug.Log ("fewf" + System.DateTime.Now);
+	}
+}
