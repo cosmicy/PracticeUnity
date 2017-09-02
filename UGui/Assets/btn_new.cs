fc563bfa4b4f4e8c9986cb5aa5d55c7071af9463
cymac <cosmicy@outlook.com>
2017-09-02 11:44:49 +0800

    *添加动画控制的Image按钮

---
+++ UGui/Assets/btn_new.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class btn_new : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Click()
+	{
+		print ("ewrerw" + System.DateTime.Now);
+
+	}
+}
